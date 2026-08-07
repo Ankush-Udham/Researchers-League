11fb0f5199bd73f785e41ac2f68d682c134c527d
Ankush-Udham <ankushudham24@gmail.com>
2026-08-07 21:41:22 +0530

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL = "https://your-backend-name.onrender.com";
 export const API = `${BACKEND_URL}/api`;
 
 export const fileUrl = (storagePath) => `${API}/files/${storagePath}`;
