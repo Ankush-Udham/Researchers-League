43677e303ff8f96ff007430ce7f5037c4746fb71
Ankush-Udham <ankushudham24@gmail.com>
2026-08-08 13:47:16 +0530

    Update Rankings.js

--- frontend/src/pages/Rankings.js
+++ frontend/src/pages/Rankings.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import api from "../lib/api";
-import { SPORTS, TEAM_COLOR, PageHead } from "../components/shared";
+import { PageHead } from "../components/shared";
 
 const TABS = { OVERALL: "Overall", ...SPORTS };
 
